7041eaf6ce98b602ebaeffd800f7f5b31b0d4fa5
Heesang whang <hwhang0917@gmail.com>
2020-08-02 06:40:35 +0900

    ⚙ Add The Movie DB API

--- README.md
+++ README.md
@@ -10,6 +10,6 @@
 - [ ] Setup APIs
   - [x] [Naver API](https://developers.naver.com/docs/search/movie/)
     - 네이버 Developer API 같은 경우 Client-side에서 CORS 제한이 걸려있기 때문에 프록시 서버로 실행 [[참고]](https://msyu1207.tistory.com/65)
-  - [ ] [The Movie DB API](https://developers.themoviedb.org/)
+  - [x] [The Movie DB API](https://developers.themoviedb.org/)
+    - Issue with Axios@0.19.x, not passing params in the request -> downgraded to Axios@0.18.1
   - [ ] [KOBIS API](https://www.kobis.or.kr/kobisopenapi/homepg/apiservice/searchServiceInfo.do)
-
--- src/api.js
+++ src/api.js
@@ -11,3 +11,49 @@ const naverApi = axios.create({
 
 export const getMovieSearch = (query) =>
   naverApi.get("search/movie.json", { params: { query } });
+
+const tmdbApi = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: process.env.REACT_APP_THEMOVIEDB_APIKEY,
+    language: "ko-KR",
+  },
+});
+
+export const tmdbMoviesApi = {
+  nowPlaying: () => tmdbApi.get("movie/now_playing"),
+  upcoming: () => tmdbApi.get("movie/upcoming"),
+  popular: () => tmdbApi.get("movie/popular"),
+  movieDetail: (id) =>
+    tmdbApi.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    tmdbApi.get("search/movie", {
+      params: {
+        // themoviedb API aitomatically URIencodes search term
+        query: term,
+      },
+    }),
+};
+
+export const tmdbTVApi = {
+  topRated: () => tmdbApi.get("tv/top_rated"),
+  popular: () => tmdbApi.get("tv/popular"),
+  airingToday: () => tmdbApi.get("tv/airing_today"),
+  showDetail: (id) =>
+    tmdbApi.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    tmdbApi.get("search/tv", {
+      params: {
+        // themoviedb API aitomatically URIencodes search term
+        query: term,
+      },
+    }),
+};
