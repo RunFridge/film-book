4072d02fb563d6c3fe2deebe434c07e44b2ef0d8
Heesang whang <hwhang0917@gmail.com>
2020-08-10 17:24:24 +0900

    ⚡️ Add proptypes to FontAwesomeIcon

--- src/Components/FontAwesomeIcon.js
+++ src/Components/FontAwesomeIcon.js
@@ -1,10 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import colors from "../Style/colors";
 
 const Icon = styled.i`
   color: ${(props) => props.color};
   font-size: ${(props) => props.size};
+  &:hover {
+    color: ${(props) => props.hoverColor};
+  }
 `;
 
 const FontAwesomeIcon = (props) => {
@@ -12,9 +16,17 @@ const FontAwesomeIcon = (props) => {
     <Icon
       className={props.class}
       color={props.color ? props.color : colors.darkBg}
+      hoverColor={props.hoverColor ? props.hoverColor : props.color}
       size={props.size ? props.size : "1rem"}
     />
   ) : null;
+};
+
+FontAwesomeIcon.propTypes = {
+  className: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  hoverColor: PropTypes.string,
+  size: PropTypes.string,
 };
 
 export default FontAwesomeIcon;
