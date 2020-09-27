afb09dfe2ecfe1d25102bb8353b03270713dbd34
Heesang whang <hwhang0917@gmail.com>
2020-09-27 23:12:37 +0900

    ✨ Add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 import GlobalStyle from "./Styles/GlobalStyle";
+import { Movie } from "./@types/graphqlTypes";
+
+const GET_POPULAR_MOVIES = gql`
+  query {
+    popularMovies {
+      id
+      title
+    }
+  }
+`;
 
 const App = () => {
+  const { loading, error, data } = useQuery(GET_POPULAR_MOVIES);
   return (
     <>
       <GlobalStyle />
-      <h1>React JS + TS Migration!!</h1>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        data.popularMovies.map((m: Movie) => <h1 key={m.id}>{m.title}</h1>)
+      )}
     </>
   );
 };
