b4f35f9b7cbe0169db4d0f74aef879eb488a56bd
Heesang whang <hwhang0917@gmail.com>
2020-10-01 10:16:19 +0900

    ✨ Add basic query in home screen

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,45 @@
 import React, { ReactElement } from "react";
+import { useQuery, gql } from "@apollo/client";
 
-const Home = (): ReactElement => <h1>Home</h1>;
+// Components
+import Loading from "../Components/Loading";
+import Error from "../Components/Error";
+
+const HOME_QUERY = gql`
+  query {
+    popularMovies {
+      id
+      title
+      poster_path
+      vote_average
+    }
+    nowPlayingMovies {
+      id
+      title
+      poster_path
+      vote_average
+    }
+    upcomingMovies {
+      id
+      title
+      poster_path
+      vote_average
+    }
+  }
+`;
+
+const Home = (): ReactElement => {
+  const { loading, error, data } = useQuery(HOME_QUERY);
+  console.log(data);
+  if (loading) {
+    return <Loading />;
+  } else {
+    if (error) {
+      return <Error />;
+    } else {
+      return <h1>Home</h1>;
+    }
+  }
+};
 
 export default Home;
