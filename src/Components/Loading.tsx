d9fb55e61ec33985ceedc00e8cae1f4b7dfcdba7
Heesang whang <hwhang0917@gmail.com>
2020-10-02 01:35:58 +0900

    💄 Add Loading component style

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,6 +1,79 @@
 import React, { ReactElement } from "react";
 import styled from "styled-components";
 
-const Loading = (): ReactElement => <h1>Loading ...</h1>;
+// Type
+import { Theme } from "../@types/style";
+
+/*
+==========================
+    Styled Components
+==========================
+*/
+const LoadingWrapper = styled.div`
+  /* Size */
+  height: 85vh;
+`;
+const Loader = styled.div`
+  /* Flexbox */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* Size */
+  width: 100%;
+  height: 100%;
+
+  /* Imported from loading.io/css */
+  .lds-ripple {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ripple div {
+    position: absolute;
+    border: 4px solid ${({ theme }: { theme: Theme }) => theme.primary};
+    opacity: 1;
+    border-radius: 50%;
+    animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+  .lds-ripple div:nth-child(2) {
+    animation-delay: -0.5s;
+  }
+  @keyframes lds-ripple {
+    0% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 1;
+    }
+    100% {
+      top: 0px;
+      left: 0px;
+      width: 72px;
+      height: 72px;
+      opacity: 0;
+    }
+  }
+`;
+
+/*
+==========================
+    React Element
+==========================
+*/
+const Loading = (): ReactElement => {
+  return (
+    <LoadingWrapper>
+      <Loader>
+        <div className="lds-ripple">
+          <div></div>
+          <div></div>
+        </div>
+      </Loader>
+    </LoadingWrapper>
+  );
+};
 
 export default Loading;
