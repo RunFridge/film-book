63b477d1e58c3f99fe3c22c686eea8379098633e
Heesang whang <hwhang0917@gmail.com>
2020-10-06 23:21:34 +0900

    💄 Update MovieDetail UI

--- src/Pages/MovieDetail.tsx
+++ src/Pages/MovieDetail.tsx
@@ -11,9 +11,11 @@ import { constructImageUri, backdropSizes, posterSizes } from "../Utils";
 import Loading from "../Components/Loading";
 import Error from "../Components/Error";
 import Slider from "../Components/Slider";
+import Thumbnail from "../Components/Thumbnail";
 
 // Type
 import { Theme } from "../@types/style";
+import { Movie } from "../@types/graphqlTypes";
 
 /*
 ==========================
@@ -107,34 +109,56 @@ const Contents = styled.div`
   /* Size */
   margin: 15px 0;
 `;
-const Cover = styled.img`
+
+const CoverContainer = styled.div`
   /* Display */
   display: none;
 
   /* Size */
   padding: 0 30px;
-  width: 70vw;
 
+  /* Transition */
+  transition: 0.25s ease-out;
   /* Responsive */
   ${device.desktop} {
     display: block;
   }
 `;
 
+const Cover = styled.img`
+  /* Border */
+  border-radius: 2em;
+
+  /* Transition */
+  transition: all 250ms ease-out;
+`;
+
 const TextContainer = styled.div`
   /* Size */
   padding: 0 30px;
 `;
+
 const Title = styled.h1`
   /* Border */
   border-bottom: 1px solid;
 
   /* Font Style */
-  font-size: 2.5em;
+  font-size: 2.8em;
   font-weight: 700;
   border-bottom: 0.5px solid ${({ theme }: { theme: Theme }) => theme.disabled};
   text-shadow: 2px 4px 3px ${({ theme }: { theme: Theme }) => theme.textShadow};
 `;
+
+const InfoContainer = styled.div`
+  /* Flexbox */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  /* Size */
+  width: 100%;
+`;
+
 const Info = styled.ul`
   /* Flexbox */
   display: flex;
@@ -142,7 +166,7 @@ const Info = styled.ul`
 
   /* Size */
   padding: 0;
-  margin: 10px 0;
+  padding: 10px 0;
 
   /* Style */
   list-style-type: none;
@@ -158,7 +182,48 @@ const Info = styled.ul`
   }
 `;
 const InfoItem = styled.li``;
-const Overview = styled.p``;
+
+const Rating = styled.h2`
+  /* Flexbox */
+  display: flex;
+  align-items: center;
+
+  /* Font Style */
+  color: ${({ theme }: { theme: Theme }) => theme.primary};
+
+  /* Size */
+  padding: 15px 0;
+  & > i {
+    margin-right: 5px;
+  }
+`;
+
+const Overview = styled.p`
+  /* Size */
+  padding: 15px 0;
+
+  /* Font Style */
+  font-size: 1.25em;
+  line-height: 1.5em;
+`;
+
+const VideosContainer = styled.div`
+  /* Gridbox */
+  display: grid;
+  grid-template-columns: repeat(5, minmax(70px, 1fr));
+  grid-gap: 10px;
+`;
+
+const ContainerTitle = styled.h3`
+  /* Font style */
+  font-size: 1em;
+  font-weight: 700;
+  border-bottom: 0.5px solid ${({ theme }: { theme: Theme }) => theme.disabled};
+  text-shadow: 2px 4px 3px ${({ theme }: { theme: Theme }) => theme.textShadow};
+
+  /* Size */
+  margin-bottom: 15px;
+`;
 
 /*
 ==========================
@@ -182,13 +247,19 @@ const MovieDetail = withRouter(
       if (error) {
         return <Error message={error.message} />;
       } else {
-        const { movieDetail, similarMovies } = data;
+        const {
+          movieDetail,
+          similarMovies,
+        }: { movieDetail: Movie; similarMovies: [Movie] } = data;
         const {
           credits: { cast, crew },
+          videos: { results: videos },
         } = movieDetail;
+
+        // RENDER
         return (
           <>
-            {/* Render backdrop if exists */}
+            {/* ====== Render backdrop if exists */}
             {movieDetail.backdrop_path && (
               <Backdrop
                 bgImage={constructImageUri(
@@ -198,41 +269,73 @@ const MovieDetail = withRouter(
               />
             )}
 
+            {/* ====== Render Contents ====== */}
             <Contents>
-              <Cover
-                src={
-                  movieDetail.poster_path
-                    ? constructImageUri(
-                        movieDetail.poster_path,
-                        posterSizes.w500
-                      )
-                    : require("../Assets/noPosterSmall.png")
-                }
-              />
+              {/* ====== Poster Cover ====== */}
+              <CoverContainer>
+                <Cover
+                  src={
+                    movieDetail.poster_path
+                      ? constructImageUri(
+                          movieDetail.poster_path,
+                          posterSizes.w500
+                        )
+                      : require("../Assets/noPosterSmall.png")
+                  }
+                />
+              </CoverContainer>
+
+              {/* ====== Text Information ====== */}
               <TextContainer>
                 <Title>{movieDetail.title}</Title>
-                <Info>
-                  <InfoItem>{movieDetail.release_date}</InfoItem>
-                  <InfoItem>{movieDetail.runtime} min</InfoItem>
-                  {movieDetail.imdb_id && (
-                    <a
-                      href={`https://www.imdb.com/title/${movieDetail.imdb_id}/`}
-                      target="_blank"
-                    >
-                      <img
-                        src={require("../Assets/imdbLogo.svg")}
-                        alt="IMDB Link"
-                        width="40px"
-                      />
-                    </a>
-                  )}
-                </Info>
+                <InfoContainer>
+                  <Info>
+                    <InfoItem>{movieDetail.release_date}</InfoItem>
+                    <InfoItem>{movieDetail.runtime}분</InfoItem>
+                    {movieDetail.imdb_id && (
+                      <a
+                        href={`https://www.imdb.com/title/${movieDetail.imdb_id}/`}
+                        target="_blank"
+                      >
+                        <img
+                          src={require("../Assets/imdbLogo.svg")}
+                          alt="IMDB Link"
+                          width="40px"
+                        />
+                      </a>
+                    )}
+                  </Info>
+                  <Info>
+                    {movieDetail.genres.map((genre, idx) => (
+                      <InfoItem key={idx}>{genre.name}</InfoItem>
+                    ))}
+                  </Info>
+                </InfoContainer>
+                {movieDetail.vote_average && (
+                  <Rating>
+                    <i className="fas fa-star" />
+                    평점: {movieDetail.vote_average} / 10
+                  </Rating>
+                )}
                 <Overview>{movieDetail.overview}</Overview>
+                {videos && videos.length > 0 && (
+                  <>
+                    <ContainerTitle>관련 영상</ContainerTitle>
+                    <VideosContainer>
+                      {videos.map((video, idx) =>
+                        video.site === "YouTube" ? (
+                          <Thumbnail key={idx} video={video} />
+                        ) : null
+                      )}
+                    </VideosContainer>
+                  </>
+                )}
               </TextContainer>
             </Contents>
-            {cast.length !== 0 && <Slider people={cast} sliderTitle="캐스트" />}
-            {crew.length !== 0 && <Slider people={crew} sliderTitle="크루" />}
-            {similarMovies.length !== 0 && (
+            {/* ====== Sliders ====== */}
+            {cast.length > 0 && <Slider people={cast} sliderTitle="캐스트" />}
+            {crew.length > 0 && <Slider people={crew} sliderTitle="크루" />}
+            {similarMovies.length > 0 && (
               <Slider movies={similarMovies} sliderTitle="관련 영화" />
             )}
           </>
